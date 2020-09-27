c33d306a0a62f2ae040ba7f6dd26cbe38b4bd277
李佳坤 <jiakun.ui@gmail.com>
2020-09-27 14:52:51 +0800

    修改说明

--- 10010/10010_cookie.js
+++ 10010/10010_cookie.js
@@ -1,5 +1,5 @@
 /*
-@LJJJia 借鉴 @GideonSenku、@chavyleung 两位大佬的脚本
+@LJJJia 借助了 @GideonSenku、@chavyleung 两位大佬的脚本
 
 使用方法：
 添加 MITM,添加 SCRIPT,自行修改所需task执行时间，示例为每日早8点和晚7点提醒。
@@ -12,17 +12,18 @@
 [MITM]
 hostname = *.10010.com
 
-**Surge**
+**Surge************
 [Script]
 10010余额提醒cookie = type=http-request, pattern=^https:\/\/m\.client\.10010\.com\/mobileService\/home\/queryUserInfoSeven\.htm, script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010_cookie.js
 10010余额提醒 = type=cron,cronexp="0 8,19 * * *",wake-system=1,script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010.js,script-update-interval=0
 
-**QuanX**
+**QuanX************
 [rewrite_local]
 ^https:\/\/m\.client\.10010\.com\/mobileService\/home\/queryUserInfoSeven\.htm url script-request-header https://raw.githubusercontent.com/LJJJia/script/master/10010/10010_cookie.js
 [task_local]
 0 8,19 * * * https://raw.githubusercontent.com/LJJJia/script/master/10010/10010.js, tag=10010余额提醒, enabled=true
-**Loon**
+
+**Loon************
 [Script]
 http-request ^https:\/\/m\.client\.10010\.com\/mobileService\/home\/queryUserInfoSeven\.htm script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010_cookie.js, timeout=10, tag=10010余额提醒cookie
 cron "0 8,19 * * *" script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010.js, tag=10010余额提醒
