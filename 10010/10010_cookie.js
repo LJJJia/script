dd77d792df6efc55fea01b74da6bb0c2cea88323
李佳坤 <jiakun.ui@gmail.com>
2020-09-27 14:02:42 +0800

    更新注释

--- 10010/10010_cookie.js
+++ 10010/10010_cookie.js
@@ -1,3 +1,33 @@
+/*
+@LJJJia 借鉴 @GideonSenku、@chavyleung 两位大佬的脚本
+
+使用方法：
+添加 MITM,添加 SCRIPT,自行修改所需task执行时间，示例为每日早8点和晚7点提醒。
+
+获取cookie方法：
+打开联通“手机营业厅”APP，即可获取cookie
+
+配置：
+
+[MITM]
+hostname = *.10010.com
+
+**Surge**
+[Script]
+10010余额提醒cookie = type=http-request, pattern=^http\:\/\/m\.client\.10010\.com\/mobileService\/home\/queryUserInfoSeven\.htm, script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010_cookie.js
+10010余额提醒 = type=cron,cronexp="0 8,19 * * *",wake-system=1,script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010.js,script-update-interval=0
+
+**QuanX**
+[rewrite_local]
+^http\:\/\/m\.client\.10010\.com\/mobileService\/home\/queryUserInfoSeven\.htm url script-request-header https://raw.githubusercontent.com/LJJJia/script/master/10010/10010_cookie.js
+[task_local]
+0 8,19 * * * https://raw.githubusercontent.com/LJJJia/script/master/10010/10010.js, tag=10010余额提醒, enabled=true
+**Loon**
+[Script]
+http-request ^http\:\/\/m\.client\.10010\.com\/mobileService\/home\/queryUserInfoSeven\.htm script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010_cookie.js, timeout=10, tag=10010余额提醒cookie
+cron "0 8,19 * * *" script-path=https://raw.githubusercontent.com/LJJJia/script/master/10010/10010.js, tag=10010余额提醒
+*/
+
 const cookieName = '联通话费提醒'
 const signurlKey = 'senku_signurl_10010'
 const signheaderKey = 'senku_signheader_10010'
